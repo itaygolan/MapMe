5966559845e68d281843d2c952d0a7e8c2332a57
Itay Golan <itaygolan@Itays-MacBook-Pro.local>
2018-07-04 09:24:07 +1200

    setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <a href='http://localhost:8888'> Login to Spotify </a>
+      </div>
+    );
+  }
+}
+
+export default App;
